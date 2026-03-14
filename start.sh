6be128124021740558734096485937d6a347543b
grizzlywild <grizzlywildcat50@gmail.com>
2026-03-15 12:42:13 +1300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# 1. Start the fake web server to keep Render awake
+node -e "require('http').createServer((req, res) => res.end('Alive')).listen(10000);" &
+
+# 2. Lock in your permanent configuration
+openclaw setup
+openclaw config set gateway.mode local
+openclaw config set channels.telegram.dmPolicy allowlist
+openclaw config set channels.telegram.allowFrom '["5658658059"]'
+
+# 3. Fire up the engine
+openclaw gateway run
