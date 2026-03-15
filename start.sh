ac26bb1fde9b6eaa09fe8179adc94498e9608335
grizzlywild <grizzlywildcat50@gmail.com>
2026-03-15 13:54:02 +1300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,10 +4,9 @@
 node -e "require('http').createServer((req, res) => res.end('Alive')).listen(10000);" &
 
 # 2. Lock in your permanent configuration
-openclaw setup
 openclaw config set gateway.mode local
 openclaw config set channels.telegram.dmPolicy allowlist
-openclaw config set channels.telegram.allowFrom '["5658658059"]'
+openclaw config set channels.telegram.allowFrom "[\"5658658059\"]"
 
 # 3. Fire up the engine
 openclaw gateway run
